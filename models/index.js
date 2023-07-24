1348f6dc32d973f1722c41f4d9ae4db8bed22e75
leamr10 <leamr10@gmail.com>
2023-07-24 17:19:11 -0400

    associations added

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsto(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsto(ProductTag, {
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsto(ProductTag, {
+  foreignKey: 'product_id',
+})
 
 module.exports = {
   Product,
